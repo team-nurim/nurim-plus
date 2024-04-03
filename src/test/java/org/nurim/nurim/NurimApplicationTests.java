e67cbdea9dd77222b53e504eef188f472f1eb446
DESKTOP-BI5BBNV\hi <mangoperry2015@gmail.com>
2024-04-03 13:06:20 +0900

    initial commit

---
+++ src/test/java/org/nurim/nurim/NurimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nurim.nurim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
