4895c9867322ceb14736b2e56cba56881caa84ae
thdrkfka <ssongjiaran@gmail.com>
2024-04-08 19:27:43 +0900

    [Hotfix] 관리자 회원관리 중복 부분 세부사항 - 관리자 회원관리 crud 작업 중 중복되는 부분 있어서 중복되지 않은 부분만 따로 가져옴. - 회원 전환은 자격증 이미지를 받게 되면 vue 에서 type을 변환하는 로직으로 진행 예정
    할일목록

--- src/main/java/org/nurim/nurim/Controller/AdminController.java
+++ src/main/java/org/nurim/nurim/Controller/AdminController.java
@@ -1,4 +1,36 @@
 package org.nurim.nurim.Controller;
 
+import org.nurim.nurim.domain.entity.Member;
+import org.nurim.nurim.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/members")
 public class AdminController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping("/")
+    public List<Member> getAllMembers() {
+        return adminService.getAllMembers();
+    }
+
+    @GetMapping("/paged")
+    public Page<Member> getAllMembersPaged(@RequestParam(defaultValue = "0") int page) {
+        return adminService.getAllMembers(page);
+    }
+
+
 }
--- src/main/java/org/nurim/nurim/service/AdminService.java
+++ src/main/java/org/nurim/nurim/service/AdminService.java
@@ -1,4 +1,31 @@
 package org.nurim.nurim.service;
 
+import lombok.RequiredArgsConstructor;
+import org.nurim.nurim.domain.entity.Member;
+import org.nurim.nurim.repository.MemberRepository;
+import org.nurim.nurim.repository.PostRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AdminService {
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
+
+    // 페이징 처리
+    public Page<Member> getAllMembers(int page) {
+        // 기본 페이지 크기
+        int pageSize = 20;
+        PageRequest pageRequest = PageRequest.of(page - 1, pageSize);
+        return memberRepository.findAll(pageRequest);
+    }
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+
 }
