e1ee925cc204c22d25119337d974298ea5fa4376
thdrkfka <ssongjiaran@gmail.com>
2024-04-12 10:05:50 +0900

    [Hotfix] mypage 푸시

--- src/main/java/org/nurim/nurim/service/MemberService.java
+++ src/main/java/org/nurim/nurim/service/MemberService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
