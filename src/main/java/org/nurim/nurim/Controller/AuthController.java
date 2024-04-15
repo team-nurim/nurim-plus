da2cf07e7f8f8c8c37997ed1e71115c4596e62fb
ChaeYeon Park <mangoperry2015@gmail.com>
2024-04-15 18:29:27 +0900

    [fix] 인증 객체 에러 수정

--- src/main/java/org/nurim/nurim/Controller/AuthController.java
+++ src/main/java/org/nurim/nurim/Controller/AuthController.java
@@ -1,5 +1,6 @@
 package org.nurim.nurim.Controller;
 
+import com.google.gson.Gson;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
 import org.nurim.nurim.config.auth.TokenProvider;
@@ -39,42 +40,14 @@ public class AuthController {
         return Arrays.asList("AAA", "BBB", "CCC");
     }
 
+
 //    @PostMapping("/login")
-//    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request) {
-//        return authService.login(request);
+//    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+//
+//
+//        return ResponseEntity.ok(new LoginResponse(jsonStr));
 //    }
 
-
-//    @PostMapping("/generateToken")
-//    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
-//
-//        // 사용자 인증 (실제 구현은 데이터베이스 검색 등이 필요합니다)
-//        Authentication authentication = authenticationManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(
-//                        loginRequest.getMemberEmail(),
-//                        loginRequest.getMemberPw()
-//                )
-//        );
-//
-//        // 인증 성공시 SecurityContext에 인증 객체 저장
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//
-//        // 토큰 생성에 필요한 정보 저장
-//        Map<String, Object> valueMap = new HashMap<>();
-//        valueMap.put("princial", authentication.getPrincipal());
-//        valueMap.put("authorities", authentication.getAuthorities());
-//
-//        // JWT 토큰 생성
-//        String accessToken = tokenProvider.generateToken(valueMap, 1);
-//        String refreshToken = tokenProvider.generateToken(valueMap, 30);
-//
-//        // 응답 header에 토큰값 설정
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("Authorization", "Bearer " + accessToken);
-//        headers.set("Refresh", "Bearer " + refreshToken);
-//
-//        return new ResponseEntity<>(new LoginResponse(accessToken, refreshToken), headers, HttpStatus.OK);
-//    }
 
     @PostMapping("/logout")
     public ResponseEntity<?> logout() {
--- src/main/java/org/nurim/nurim/Controller/MemberController.java
+++ src/main/java/org/nurim/nurim/Controller/MemberController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.nurim.nurim.domain.dto.member.*;
+import org.nurim.nurim.domain.entity.Member;
 import org.nurim.nurim.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +20,9 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @Operation(summary = "개인 정보 입력")
-    @PostMapping
-    public ResponseEntity<CreateMemberResponse> memberInfoCreate(@RequestBody @Valid CreateMemberRequest request){
+    @Operation(summary = "일반 회원 등록")
+    @PostMapping("/user")
+    public ResponseEntity<CreateMemberResponse> memberCreate(@RequestBody @Valid CreateMemberRequest request){
 
         CreateMemberResponse response = memberService.createMember(request);
 
@@ -29,40 +30,94 @@ public class MemberController {
 
     }
 
-    @Operation(summary = "개인 정보 단건 조회")
+    @Operation(summary = "관리자 회원 등록")
+    @PostMapping("/admin")
+    public ResponseEntity<CreateMemberResponse> adminCreate(@RequestBody @Valid CreateMemberRequest request){
+
+        CreateMemberResponse response = memberService.createAdmin(request);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
+    @Operation(summary = "회원 정보 입력")
+    @PostMapping("/memberInfo")
+    public ResponseEntity<CreateMemberResponse> memberInfoCreate(@RequestBody @Valid CreateMemberInfoRequest request) {
+
+        CreateMemberResponse response = memberService.createMemberInfo(request);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+    @Operation(summary = "회원 정보 단건 조회")
     @GetMapping("/{memberId}")
     public ResponseEntity<ReadMemberResponse> memberReadById(@PathVariable Long memberId) {
+
         ReadMemberResponse response = memberService.readMemberById(memberId);
 
         return  new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @Operation(summary = "개인 정보 수정")
-    @PutMapping("/{memberId}")
-    public ResponseEntity<UpdateMemberResponse> memberInfoUpdate(@PathVariable Long memberId, @RequestBody UpdateMemberRequest request){
+    // 💌 검토 필요 (추가)
+    @Operation(summary = "JWT를 통한 Mypage 정보 불러오기")
+    @GetMapping("/mypage")
+    public ResponseEntity<ReadMemberResponse> getMyInfo(){
 
-        UpdateMemberResponse response = memberService.updateMember(memberId, request);
+        Member accessMember = memberService.getMember();
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    @Operation(summary = "개인 정보 삭제") // 회원가입이 이뤄지면 email에 대한 정보로 탈퇴 처리해야 할 듯
-    @DeleteMapping("/{memberId}")
-    public ResponseEntity<DeleteMemberResponse> memberInfoDelete(@PathVariable Long memberId){
-
-        DeleteMemberResponse response = memberService.deleteMember(memberId);
+        ReadMemberResponse response = memberService.readMemberById(accessMember.getMemberId());
 
         return new ResponseEntity<>(response, HttpStatus.OK);
 
     }
 
-//    @Operation(summary = "개인 정보 일부 수정") // 아직 미완
-//    @PatchMapping("/{memberId}")
-//    public ResponseEntity<PatchMemberResponse> memberPartUpdate(@PathVariable Long memberId, @RequestBody PatchMemberRequest request) {
+    // 다른 회원 프로필 조회
+//    @Operation(summary = "다른 회원 프로필 정보 조회")
+//    @GetMapping("/user/{username}")
+//    public ResponseEntity<ReadMemberResponse> readMemberByMemberEmail(@PathVariable String memberEmail) {
 //
-//        PatchMemberResponse response = memberService.updateMemberPart(memberId, request);
+//        Member targetMember = memberService.readMemberByMemberEmail(memberEmail);
+//
+//        ReadMemberResponse response = memberService.readMemberById(targetMember.getMemberId());
 //
 //        return new ResponseEntity<>(response, HttpStatus.OK);
+//
 //    }
+
+//    @Operation(summary = "회원 정보 삭제") // 회원가입이 이뤄지면 email에 대한 정보로 탈퇴 처리해야 할 듯
+//    @DeleteMapping("/{memberId}")
+//    public ResponseEntity<DeleteMemberResponse> memberInfoDelete(@PathVariable Long memberId){
+//
+//        DeleteMemberResponse response = memberService.deleteMember(memberId);
+//
+//        return new ResponseEntity<>(response, HttpStatus.OK);
+//
+//    }
+
+    // 💌 검토 필요 (로그인한 사용자만 회원탈퇴 가능)
+    @Operation(summary = "회원 정보 삭제") // 회원가입이 이뤄지면 email에 대한 정보로 탈퇴 처리해야 할 듯
+    @DeleteMapping
+    public ResponseEntity<DeleteMemberResponse> memberDelete(){
+
+        Member accessMember = memberService.getMember();
+
+        DeleteMemberResponse response = memberService.deleteMember(accessMember.getMemberId());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+
+    // 💌 검토 필요 (로그인한 사용자가 본인 정보만 수정 가능)
+    @Operation(summary = "회원 정보 수정")
+    @PutMapping("/{memberId}")
+    public ResponseEntity<UpdateMemberResponse> memberUpdate(@RequestBody UpdateMemberRequest request) {
+
+        Member accessMember = memberService.getMember();
+
+        UpdateMemberResponse response = memberService.updateMember(accessMember.getMemberId(), request);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 
 }
