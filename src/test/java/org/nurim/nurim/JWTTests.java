e239a4b48123dce71df2e3625f92e1267e79ea70
thdrkfka <ssongjiaran@gmail.com>
2024-04-17 18:07:52 +0900

    [Hotfix] develop pull 받음

--- src/test/java/org/nurim/nurim/JWTTests.java
+++ src/test/java/org/nurim/nurim/JWTTests.java
@@ -1,22 +1,14 @@
 package org.nurim.nurim;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.nurim.nurim.config.auth.TokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Map;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
