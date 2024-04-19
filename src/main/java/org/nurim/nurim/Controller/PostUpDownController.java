ebbf9bc0f1095d95d7f35886f64dd6071d95ad74
thdrkfka <ssongjiaran@gmail.com>
2024-04-19 09:40:17 +0900

    [Hotfix] post image 수정 완료 세부사항 - @Transactional 이슈로 DB에 접근이 어려워서 저장, 삭제 로직이 안되었으나 수정함. - 수정 사항 : controller 에서 전역으로 @Transactional(readonly = true)이 걸려 있어서 기능이 제대로 수행되지 않았던 상황이어서 어노테이션 지워줌.

--- src/main/java/org/nurim/nurim/Controller/PostUpDownController.java
+++ src/main/java/org/nurim/nurim/Controller/PostUpDownController.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,7 +24,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Log4j2
 public class PostUpDownController {
