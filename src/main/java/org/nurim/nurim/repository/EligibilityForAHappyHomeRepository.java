2ed4e76c19a47393f06dd7ce2ca1a0ffce3445a3
EuniceYou12 <euni.you@gmail.com>
2024-04-25 18:11:03 +0900

    [feat] view policy update create controller, service repository and entity for Housing Policy and Integrated Policy

--- src/main/java/org/nurim/nurim/repository/EligibilityForAHappyHomeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.nurim.nurim.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EligibilityForAHappyHomeRepository extends JpaRepository<EligibilityForAHappyHome, Long> {
-
-}
--- src/main/java/org/nurim/nurim/repository/HtmlTableRepository.java
+++
@@ -1,6 +0,0 @@
-package org.nurim.nurim.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HtmlTableRepository extends JpaRepository<HtmlTable, Long> {
-}
